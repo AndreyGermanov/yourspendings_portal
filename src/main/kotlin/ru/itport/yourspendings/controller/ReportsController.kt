4a800ed990689beec32f0499690d6de46c5a1fd6
Andrey Germanov <andrey@it-port.ru>
2019-03-24 23:54:03 +0300

    Hierarchial rows groupping implementation for Reports Builder

--- src/main/kotlin/ru/itport/yourspendings/controller/ReportsController.kt
+++ src/main/kotlin/ru/itport/yourspendings/controller/ReportsController.kt
@@ -45,6 +45,7 @@ class ReportsController:EntityController<Report>("Report") {
             list.removeAt(list.size - 1)
         }
     }
+
     @PostMapping("/generate")
     fun generate(@RequestBody body:Any=ArrayList<MutableMap<String,Any>>()):Any? {
         val requests = this.parseReportRequest(body) ?: return null
@@ -61,7 +62,8 @@ class ReportsController:EntityController<Report>("Report") {
                 var resultRow = ArrayList<Any?>()
                 var row = row as Array<Any>
                 if (request.format.groups.isNotEmpty()) {
-                    request.format.groups.forEachIndexed { groupFieldIndex,groupFieldValue ->
+                    request.format.groups.forEachIndexed { groupFieldIndex,groupFormat ->
+                        var groupFieldValue = groupFormat.fieldIndex
                         if (currentGroups.size - 1 < groupFieldIndex ||
                                 currentGroups[groupFieldIndex]["value"].toString() != row[groupFieldValue].toString()) {
                             if (currentGroups.size > groupFieldIndex) {
@@ -70,13 +72,21 @@ class ReportsController:EntityController<Report>("Report") {
                             var groupRow =  ArrayList<Any>(row.size+1)
                             row.forEach { groupRow.add("")}
                             groupRow.add("")
-                            resultReport.add(groupRow)
+                            if (currentGroups.size>0) {
+                                val parentGroupRow = currentGroups[currentGroups.size-1]["row"] as ArrayList<Any>
+                                (parentGroupRow[parentGroupRow.size-1] as ArrayList<Any>).add(groupRow)
+                            } else {
+                                resultReport.add(groupRow)
+                            }
                             currentGroups.add(hashMapOf("value" to row[groupFieldValue].toString(),
-                                    "rowIndex" to resultReport.size-1))
+                                    "row" to groupRow))
                             request.format.groups.forEachIndexed { idx, value ->
-                                if (idx<=groupFieldIndex) groupRow[idx] = row[value]
+                                if (idx<=groupFieldIndex) groupRow[idx] = row[value.fieldIndex]
                             }
                             groupRow[row.size] = hashMapOf("groupLevel" to groupFieldIndex, "groupColumn" to groupFieldValue)
+                            if (groupFormat.isHierarchy)
+                                groupRow[groupFormat.hierarchyIdField] = row[groupFormat.hierarchyIdField]
+                            groupRow.add(ArrayList<Any>())
                         }
                     }
                 }
@@ -85,10 +95,10 @@ class ReportsController:EntityController<Report>("Report") {
                     resultRow.add(value)
                     if (column.groupFunction !== null) {
                         currentGroups.forEach {
-                            var groupRow = resultReport[it["rowIndex"].toString().toInt()] as ArrayList<Any>
+                            var groupRow = it["row"] as ArrayList<Any>
                             var currentValue = convertFieldValue(request.format.columns[index],groupRow[index])
                             var currentValueDbl = currentValue.toString().toDouble()
-                            var valueDbl = value.toString().toString().toDouble()
+                            var valueDbl = value.toString().toDouble()
                             currentValue = when (column.groupFunction) {
                                 GroupFunction.avg -> if (currentValueDbl>0) (valueDbl+currentValueDbl)/2 else valueDbl
                                 GroupFunction.max -> if (valueDbl>currentValueDbl) valueDbl else currentValue
@@ -98,15 +108,67 @@ class ReportsController:EntityController<Report>("Report") {
                                 GroupFunction.last -> valueDbl
                                 else -> valueDbl
                             }
-                            groupRow[index] = currentValue.toString().toDoubleOrNull() ?: 0.0
+                            groupRow[index] = convertFieldValue(request.format.columns[index],
+                                    currentValue.toString().toDoubleOrNull() ?: 0.0)!!
                         }
                     }
                 }
                 resultRow.add(HashMap<String,Any>())
-                resultReport.add(resultRow)
+                if (currentGroups.size>0) {
+                    var groupRow = currentGroups[currentGroups.size-1]["row"] as ArrayList<Any>
+                    (groupRow[groupRow.size-1] as ArrayList<Any>).add(resultRow)
+                } else {
+                    resultReport.add(resultRow)
+                }
             }
+            if (request.format.groups.size>0)
+                resultReport = processGroup(0,request.format.groups[0],resultReport)
             result.add(resultReport)
         }
+        return result
+    }
+
+    fun processGroup(level:Int,groupConfig:GroupFormat,items:ArrayList<Any>):ArrayList<Any> {
+        if (!groupConfig.isHierarchy) return items
+        val result = ArrayList<Any>()
+        var groups = HashMap<Int,Any>()
+        items.forEach{
+            val row = it as ArrayList<Any>
+            val parentList = getGroupParentList(row[groupConfig.hierarchyIdField].toString().toInt(),
+                    groupConfig.hierarchyModelName,groupConfig.hierarchyNameField)
+            parentList.forEach {
+                var groupRow = row
+                groupRow[groupConfig.fieldIndex] = it["name"].toString()
+                groupRow[groupRow.size-1] = ArrayList<Any>()
+                if (!groups.containsKey(it["id"].toString().toInt()))
+                    groups[it["id"].toString().toInt()] = groupRow
+                else
+                    groupRow = groups[it["id"].toString().toInt()] as ArrayList<Any>
+
+            }
+        }
+        return items
+
+    }
+
+    fun getGroupParentList(id:Int,modelName:String,nameField:String):ArrayList<HashMap<String,Any>> {
+        val item = entityManager.find(Class.forName("ru.itport.yourspendings.entity.$modelName"),id)
+        var field = item.javaClass.getDeclaredField("parent")
+        field.isAccessible = true
+        val result = ArrayList<HashMap<String,Any>>()
+        var parent = field.get(item)
+        while (parent != null) {
+            var uidField = parent.javaClass.getDeclaredField("uid")
+            var textField = parent.javaClass.getDeclaredField(nameField)
+            uidField.isAccessible = true
+            textField.isAccessible = true
+            result.add(hashMapOf("id" to uidField.get(parent),
+                    "name" to textField.get(parent)));
+            var field = parent.javaClass.getDeclaredField("parent")
+            field?.isAccessible = true
+            parent = field?.get(parent)
+        }
+        result.reverse()
         return result
     }
 
@@ -142,13 +204,26 @@ class ReportsController:EntityController<Report>("Report") {
                                         else null
                 )
             } ?: ArrayList()
-            this.groups = format["groups"] as? ArrayList<Int> ?: ArrayList()
+            this.groups = (format["groups"] as? ArrayList<HashMap<String,Any>> ?: ArrayList()).map {
+                GroupFormat(
+                    fieldIndex = it["fieldIndex"].toString().toIntOrNull() ?: 0,
+                    isHierarchy = it.containsKey("hierarchy")
+                ).apply {
+                    if (this.isHierarchy) {
+                        val options = it["hierarchy"] as HashMap<String,Any>
+                        this.hierarchyIdField = options["idField"].toString().toIntOrNull() ?: 0
+                        this.hierarchyNameField = options["nameField"]?.toString() ?: ""
+                        this.hierarchyModelName = options["entity"]?.toString() ?: ""
+                    }
+                }.also { println(it)}
+            }
         }
     }
 
     fun convertFieldValue(format:ReportRequestColumnFormat,value:Any?):Any? {
         return when (format.type) {
             "decimal" -> convertDecimalValue(format,value)
+            "integer" -> value.toString().toIntOrNull() ?: 0
             "date","datetime","time","timestamp" -> convertDateTimeValue(format,value)
             else -> value
         }
@@ -186,7 +261,7 @@ data class ReportRequest (
 data class ReportRequestOutputFormat (
     var title: String = "",
     var columns: List<ReportRequestColumnFormat> = ArrayList(),
-    var groups: List<Int> = ArrayList()
+    var groups: List<GroupFormat> = ArrayList()
 )
 
 data class ReportRequestColumnFormat (
@@ -204,4 +279,12 @@ enum class GroupFunction {
 data class GroupRow (
     var groupLevel:String,
     var totals: HashMap<Int,HashMap<GroupFunction,Double>> = HashMap()
+)
+
+data class GroupFormat (
+    var fieldIndex:Int,
+    var isHierarchy: Boolean = false,
+    var hierarchyIdField: Int = 0,
+    var hierarchyNameField:String = "",
+    var hierarchyModelName:String = ""
 )
